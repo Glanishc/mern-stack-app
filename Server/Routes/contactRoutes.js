c76fd76127c3decaba6b345376d6a84bdfd58db5
Glanish <59740247+Glanishc@users.noreply.github.com>
2025-11-05 13:36:36 +0400

    Update contactRoutes.js

--- Server/Routes/contactRoutes.js
+++ Server/Routes/contactRoutes.js
@@ -1,4 +1,14 @@
 const express = require('express')
+app.use(cors({
+    origin: [
+        'https://mern-frontend-app.azurewebsites.net',
+        'http://localhost:3000',
+        'http://localhost:5173'
+    ],
+    credentials: true
+}));
+
+app.use(express.json());
 const { submitMessage } = require('../Controllers/contactCtrl')
 
 const contactRouter = express.Router()
