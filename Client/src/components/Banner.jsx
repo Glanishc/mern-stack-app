ef528c8d2430868a058fb6c33704db7feaca523d
Glanish <59740247+Glanishc@users.noreply.github.com>
2025-11-05 12:08:01 +0400

    Update Banner.jsx

--- Client/src/components/Banner.jsx
+++ Client/src/components/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
     <>
       
         <video className="back-video" loop muted autoPlay>
-          <source src="https://youtu.be/yuYom0ufoxk" type="video/mp4"/>
+          <source src="https://mernvideos.blob.core.windows.net/mern-videos/BG1.mp4" type="video/mp4"/>
         </video>
         <div className='banner'>
       </div>
