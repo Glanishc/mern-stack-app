7a3a2bfce00b9bdc0b486de4c23a14ccfc7286b9
Glanish <59740247+Glanishc@users.noreply.github.com>
2025-11-05 17:09:49 +0400

    Update vehicleRoutes.js

--- Server/Routes/vehicleRoutes.js
+++ Server/Routes/vehicleRoutes.js
@@ -1,12 +1,4 @@
 const express = require('express');
-app.use(cors({
-    origin: [
-        'https://mern-frontend-app.azurewebsites.net',
-        'http://localhost:3000',
-        'http://localhost:5173'
-    ],
-    credentials: true
-}));
 
 app.use(express.json());
 const Vehicle = require('../Model/Vehicle'); // Adjust the path as needed
