cdc1356233e10ca23064969e777fa12179f12afe
Glanish <59740247+Glanishc@users.noreply.github.com>
2025-11-07 12:39:16 +0400

    Update user.js

--- Server/Model/user.js
+++ Server/Model/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    dlno: { type: String, required: true },
+    dlno: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     attending: { type: Boolean, default: false }
