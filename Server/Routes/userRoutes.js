6062bda481c9f1b2b51a4400959135f84c23f7a1
Glanish <59740247+Glanishc@users.noreply.github.com>
2025-11-07 13:35:26 +0400

    Update userRoutes.js

--- Server/Routes/userRoutes.js
+++ Server/Routes/userRoutes.js
@@ -1,8 +1,5 @@
 const express = require('express');
-
-app.use(express.json());
 const { Register, Login, updateAttendance } = require('../Controllers/userCtrl');
-
 const userRouter = express.Router();
 
 userRouter.post('/', Register);
