b9de597c1a8bdf671547f33bf85a0ac2cc0ca934
Glanish <59740247+Glanishc@users.noreply.github.com>
2025-11-07 14:47:58 +0400

    Update Login.jsx

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
     const loginUser = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:3000/user/login', {
+            const response = await axios.post(`${import.meta.env.VITE_API_URL}/user/login`, {
                 email: email,
                 password: password
             });
